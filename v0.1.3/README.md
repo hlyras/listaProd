6b5b88408704341fdc955697aa9c576d9e71200a
hlyras <hhlyras2011@gmail.com>
2015-11-22 13:39:44 -0200

    Utilizando Metodos estaticos

--- v0.1.3/README.md
+++ v0.1.3/README.md
@@ -1,4 +1,4 @@
-Relator de Produtos --version= 0.1.0
+Relator de Produtos --version= 0.1.3
 
 Funções do Programa- 
  
--- v0.1.3/function2.js
+++ v0.1.3/function2.js
@@ -3,7 +3,7 @@
 		document.write(mensagem + "<br>");
 	}
 	//captura a quantidade e atributos dos produtos;
-	function capturarProduto(){
+	/*function capturarProduto(){
 	    qtdProduts = parseInt(prompt("Digite quantos produtos serão criados"));
 	    ids = [];
 	    for(i=0; i<qtdProduts; i++){
@@ -14,13 +14,21 @@
 	      ids.push(c1);
 	      c1.processo();
 	    }
-	}
+	}*/
+	
 	//Mostra o resultado dos processos
-	function mostrarDados(){
+	/*function mostrarDados(){
 		mostrarTela("RELATORIO DE PRODUTOS <br>");
-		mostrarTela("quantidade menor 50 = " + qtd50);
-		for(i=0; i < nomeProd.length; i++){
-			mostrarTela("Descrição produto = " +nomeProd[i]);
-		}
-		mostrarTela("media preço maior 100 = " + media100);
+		mostrarTela("Quantidade de produtos com valor inferior a 50$ = " + qtd50);
+		media50100();
+		mostrarTela("Media de preço dos produtos com valor superior a 100$ = " + media100 + "$");
 	}
+	function media50100(){
+		mostrarTela("Produtos com valor entre 50$ e 100$ <br> ")
+		for(i=0; i < nomeProd.length; i++){
+			mostrarTela("Produto = " + nomeProd[i]);
+		}
+	}
+	function medsup100(){
+		mostrarTela("Media de preço dos produtos com valor superior a 100$ = " + media100 + "$");
+	}*/
